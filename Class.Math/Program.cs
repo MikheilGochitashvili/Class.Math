bf7d83c6f48c960cd246feea92d97a28f3eb5792
მიხეილ გოჩიტაშვილი <m.gochitashvili@itstep.ge>
2022-12-28 16:10:12 +0400

    new

--- Class.Math/Program.cs
+++ Class.Math/Program.cs
@@ -12,12 +12,58 @@ namespace Class.Math
         {
             return x + y;   
         } 
+
+        public int Sub (int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Div (int x, int y)
+        {
+            return x / y;
+        }
+
+        public int Mult (int x, int y)
+        {
+            return x * y;   
+        }
+
+        public int Max (int x, int y)
+        {
+            if(x > y)
+            {
+                return x;
+            } 
+            else
+            {
+                return y;
+            }
+        }
+
+        public int Min (int x, int y)
+        {
+            if (x < y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Math math = new Math();
+
+
+            int minmax = math.Min(5, 21);
+
+            Console.WriteLine(minmax);
         }
     }
 }
