0210e1523971aae0fe3accb812a4f2539883ea15
მიხეილ გოჩიტაშვილი <m.gochitashvili@itstep.ge>
2022-12-28 15:55:37 +0400

    sd

--- Class.Math/Program.cs
+++ Class.Math/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Class.Math
 {
+    public class Math
+    {
+        public int Sum (int x, int y)
+        {
+            return x + y;   
+        } 
+    }
+
     internal class Program
     {
         static void Main(string[] args)
