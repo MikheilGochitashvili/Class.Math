bfe04549beceaa5eaaebe4531e23b44ae830bdc0
მიხეილ გოჩიტაშვილი <m.gochitashvili@itstep.ge>
2022-12-28 16:38:51 +0400

    as

--- Class.Math/Program.cs
+++ Class.Math/Program.cs
@@ -8,35 +8,48 @@ namespace Class.Math
 {
     public class Math
     {
-        public int Sum (int x, int y)
+        //Addition of two int elements
+        public int Add (int x, int y)
         {
             return x + y;   
         } 
-
+        //Subsctraction of two int elements
         public int Sub (int x, int y)
         {
             return x - y;
         }
-
+        //Division of two int elements
         public int Div (int x, int y)
         {
             return x / y;
         }
-
+        //Multiply two int elements
         public int Mult (int x, int y)
         {
             return x * y;   
         }
-
+        //Finding max value out of two passed parameter x , y
         public int Max (int x, int y)
         {
             return x > y ? x : y; 
         }
-
+        //Finding min value out of two passed parameter x , y
         public int Min (int x, int y)
         {
             return x < y ? x : y;
         }
+
+
+        //public int random (int x, int y)
+        //{
+        //    int[] array = new int[y];
+
+        //    for(int i = x; i <= y; i++)
+        //    {
+        //        array[i] = i;
+        //    }
+
+        //}
     }
 
     internal class Program
@@ -44,10 +57,47 @@ namespace Class.Math
         static void Main(string[] args)
         {
 
-            Math math = new Math();
 
- 
-            Console.WriteLine(math.Max(11, 7));
+            int x = 3;
+            int y = 12;
+
+            int Arraylength (int i, int j)
+            {
+                if(i > j)
+                {
+                    return i - j;
+                }
+                else
+                {
+                    return j - i;
+                }
+            }
+
+            Console.WriteLine(Arraylength(10, 15));
+
+            int[] arr = new int[Arraylength(x, y)];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"before : {arr[i]}");
+            }
+
+            for (int i = x; i < y; i++)
+            {
+                arr[i] = i;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"after : {arr[i]}");
+            }
+
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            Console.ReadLine();
         }
     }
 }
